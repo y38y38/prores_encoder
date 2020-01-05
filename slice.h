78f847c13699af56470a865b8daa94a63245311e
Yuusuke <y38y38@gmail.com>
2020-01-05 22:51:45 +0900

    ok

--- slice.h
+++ slice.h
@@ -13,7 +13,6 @@
 extern void set_frame_header(void);
 extern void set_picture_header(void);
 extern void encode_slices(uint16_t *y_data, uint16_t *cb_data, uint16_t *cr_data);
-//extern uint32_t picture_size_offset_;
 extern const uint8_t block_pattern_scan_table[64];
 extern uint8_t block_pattern_scan_read_order_table[64];
 
