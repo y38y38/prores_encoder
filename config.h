f277540b43093056c66414335f144cea87402643
Yuusuke <y38y38@gmail.com>
2020-11-23 18:33:51 +0900

    del malloc 2

--- config.h
+++ config.h
@@ -1,9 +1,12 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
+//it is software setting
+
 #define PRE_CALC_COS
 //#define DEL_SQRT
 #define DEL_DIVISION
 #define DEL_MULTIPLY
 #define DEL_MALLOC
+
 #endif
